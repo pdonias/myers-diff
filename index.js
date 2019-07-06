05afefba0140a97f72bd8c4b6c94e894da04767d
Pierre Donias <pierre.donias@gmail.com>
2019-07-06 19:28:42 +0200

    Minimalist list of actions

--- index.js
+++ index.js
@@ -31,14 +31,26 @@ M.forEach(r => console.log(r))
 
 let i = b.length
 let j = a.length
+let lastAction
 
 // left then up
 // -1 means left, 0 means diag, 1 means up
 const backPath = []
+const backActions = []
 while (i > 0 || j > 0) {
   if (j > 0 && M[i][j-1] === M[i][j]) {
     console.log(`${M[i][j-1]} === ${M[i][j]}`)
     backPath.push(-1)
+    if (lastAction === 'remove') {
+      backActions[backActions.length - 1].length++
+    } else {
+      backActions.push({
+        type: 'remove',
+        start: i,
+        length: 1,
+      })
+    }
+    lastAction = 'remove'
     j--
     console.log(`so we go left and proceed (${i},${j})`)
     continue
@@ -46,11 +58,22 @@ while (i > 0 || j > 0) {
   console.log(`We shall go up until we hit a corner (${i},${j})`)
   while (i > 0 && M[i-1][j] === M[i][j]) {
     backPath.push(1)
+    if (lastAction === 'add') {
+      backActions[backActions.length - 1].content = b[i-1] + backActions[backActions.length - 1].content
+    } else {
+      backActions.push({
+        type: 'add',
+        start: j,
+        content: b[i-1]
+      })
+    }
+    lastAction = 'add'
     i--
     console.log(`Up! (${i},${j})`)
   }
   if (i > 0 && j > 0) {
     backPath.push(0)
+    lastAction = undefined
     i--
     j--
     console.log(`We're taking the bridge (${i},${j})`)
@@ -58,26 +81,39 @@ while (i > 0 || j > 0) {
 }
 
 const path = backPath.reverse()
+const actions = backActions.reverse()
 
 console.log(path)
+console.log(actions)
 
 let s = a.split('')
 console.log('Process')
 console.log(s.join(''))
-i = 0
-j = 0
-path.forEach(d => {
-  if (d === -1) {
-    s.splice(i, 1)
-    console.log(s.join(''))
-  } else if (d === 0) {
-    i++
-    j++
-  } else if (d === 1) {
-    s.splice(i, 0, b[j])
-    i++
-    j++
-    console.log(s.join(''))
+
+// i = 0
+// j = 0
+// path.forEach(d => {
+//   if (d === -1) {
+//     s.splice(i, 1)
+//     console.log(s.join(''))
+//   } else if (d === 0) {
+//     i++
+//     j++
+//   } else if (d === 1) {
+//     s.splice(i, 0, b[j])
+//     i++
+//     j++
+//     console.log(s.join(''))
+//   }
+// })
+
+actions.forEach(a => {
+  if (a.type === 'add') {
+    s.splice(a.start, 0, ...a.content)
+  } else {
+    s.splice(a.start, a.length)
   }
+  console.log(s.join(''))
 })
+
 s = s.join('')
