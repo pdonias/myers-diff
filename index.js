51ee7a764a0090139b2ed15f3ebdfc5d84c0b4f6
Pierre Donias <pierre.donias@gmail.com>
2019-07-07 18:02:52 +0200

    Works

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ for (let i = 0; i < b.length + 1; i++) {
     if (i === 0 || j === 0) {
       M[i][j] = 0
     } else if (a[j-1] === b[i-1]) {
-      // console.log(`a[j]=${a[j]} and b[i]=${b[i]}`)
       M[i][j] = M[i-1][j-1] + 1
     } else {
       M[i][j] = Math.max(M[i-1][j], M[i][j-1])
@@ -27,30 +26,26 @@ for (let i = 0; i < b.length + 1; i++) {
   }
 }
 
-M.forEach(r => console.log(r))
+console.log('--- MATRIX ---')
+M.forEach(r => {
+  r.forEach(c => process.stdout.write(c + '\t'))
+  process.stdout.write('\n')
+})
+
+// -----------------------------------------------------------------------------
+
+console.log('--- DIFF ALGORITHM ---')
 
 let i = b.length
 let j = a.length
-let lastAction
 
 // left then up
 // -1 means left, 0 means diag, 1 means up
 const backPath = []
-const backActions = []
 while (i > 0 || j > 0) {
   if (j > 0 && M[i][j-1] === M[i][j]) {
     console.log(`${M[i][j-1]} === ${M[i][j]}`)
     backPath.push(-1)
-    if (lastAction === 'remove') {
-      backActions[backActions.length - 1].length++
-    } else {
-      backActions.push({
-        type: 'remove',
-        start: i,
-        length: 1,
-      })
-    }
-    lastAction = 'remove'
     j--
     console.log(`so we go left and proceed (${i},${j})`)
     continue
@@ -58,22 +53,11 @@ while (i > 0 || j > 0) {
   console.log(`We shall go up until we hit a corner (${i},${j})`)
   while (i > 0 && M[i-1][j] === M[i][j]) {
     backPath.push(1)
-    if (lastAction === 'add') {
-      backActions[backActions.length - 1].content = b[i-1] + backActions[backActions.length - 1].content
-    } else {
-      backActions.push({
-        type: 'add',
-        start: j,
-        content: b[i-1]
-      })
-    }
-    lastAction = 'add'
     i--
     console.log(`Up! (${i},${j})`)
   }
   if (i > 0 && j > 0) {
     backPath.push(0)
-    lastAction = undefined
     i--
     j--
     console.log(`We're taking the bridge (${i},${j})`)
@@ -81,39 +65,57 @@ while (i > 0 || j > 0) {
 }
 
 const path = backPath.reverse()
-const actions = backActions.reverse()
 
-console.log(path)
-console.log(actions)
+// -----------------------------------------------------------------------------
+
+const actions = []
+let p = path[0]
+let ai = 0
+i = 0, j = 0
+let sum = 0
+
+while (ai < path.length) {
+  p = path[ai]
+  while (path[ai] === p) {
+    sum++
+    ai++
+  }
+  if (p === -1) {
+    actions.push({
+      type: 'remove',
+      start: i,
+      length: sum,
+    })
+  } else if (p === 0) {
+    i += sum
+    j += sum
+  } else {
+    actions.push({
+      type: 'add',
+      start: i,
+      content: b.split('').slice(j, j + sum).join('')
+    })
+    i += sum
+    j += sum
+  }
+  sum = 0
+}
+
+console.log('--- INSTRUCTIONS ---')
+console.log('actions', actions)
+
+// -----------------------------------------------------------------------------
+
+console.log('--- EXECUTION ---')
 
 let s = a.split('')
-console.log('Process')
 console.log(s.join(''))
 
-// i = 0
-// j = 0
-// path.forEach(d => {
-//   if (d === -1) {
-//     s.splice(i, 1)
-//     console.log(s.join(''))
-//   } else if (d === 0) {
-//     i++
-//     j++
-//   } else if (d === 1) {
-//     s.splice(i, 0, b[j])
-//     i++
-//     j++
-//     console.log(s.join(''))
-//   }
-// })
-
-actions.forEach(a => {
-  if (a.type === 'add') {
-    s.splice(a.start, 0, ...a.content)
+actions.forEach(({ type, start, content, length }) => {
+  if (type === 'add') {
+    s.splice(start, 0, ...content)
   } else {
-    s.splice(a.start, a.length)
+    s.splice(start, length)
   }
   console.log(s.join(''))
 })
-
-s = s.join('')
